d11a2509aa1b8839214f3d03eddd5957ff82de8a
Till1983 <42179755+Till1983@users.noreply.github.com>
2018-09-25 23:40:53 +0200

    Add files via upload
    Mario incomplete

---
+++ pset1/mario.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int main(void)
+
+do {
+	n = get_int("Enter number: ");
+}
+while (n <= 0);
+
+for (i = 0; i < n; i++); {
+	for (i = 0; i < n; i++); {
+		printf("#");
+	}
+	printf("\n");
+}
